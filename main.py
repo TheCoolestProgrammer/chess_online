631a7d579ec7c8a01fad45003359dd390a3ff030
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-08-11 17:49:43 +0500

    selecting_chessman

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ black_color = (0,0,0)
 white_color = (255,255,255)
 process_running = True
 active_chessman = None
-
+active_cell = None
 class ChessMan:
     def __init__(self,x,y):
         self.x = x
@@ -111,26 +111,26 @@ class Field:
         for i in range(8):
             black_pawn = Pawn(i, 1, "black")
             self.black_chessmans.append(black_pawn)
-        self.field[0][0] =("w","c")
-        self.field[0][1] =("w","h")
-        self.field[0][2] =("w","b")
-        self.field[0][3] =("w","k")
-        self.field[0][4] =("w","q")
-        self.field[0][5] =("w","b")
-        self.field[0][6] =("w","h")
-        self.field[0][7] =("w","c")
-        self.field[7][0] = ("b", "c")
-        self.field[7][1] = ("b", "h")
-        self.field[7][2] = ("b", "b")
-        self.field[7][3] = ("b", "k")
-        self.field[7][4] = ("b", "q")
-        self.field[7][5] = ("b", "b")
-        self.field[7][6] = ("b", "h")
-        self.field[7][7] = ("b", "c")
+        self.field[0][0] =("b","c")
+        self.field[0][1] =("b","h")
+        self.field[0][2] =("b","b")
+        self.field[0][3] =("b","q")
+        self.field[0][4] =("b","k")
+        self.field[0][5] =("b","b")
+        self.field[0][6] =("b","h")
+        self.field[0][7] =("b","c")
+        self.field[7][0] = ("w", "c")
+        self.field[7][1] = ("w", "h")
+        self.field[7][2] = ("w", "b")
+        self.field[7][3] = ("w", "q")
+        self.field[7][4] = ("w", "k")
+        self.field[7][5] = ("w", "b")
+        self.field[7][6] = ("w", "h")
+        self.field[7][7] = ("w", "c")
         for i in range(8):
-            self.field[1][i] = ("w", "p")
+            self.field[1][i] = ("b", "p")
         for i in range(8):
-            self.field[6][i] = ("b", "p")
+            self.field[6][i] = ("w", "p")
     def draw(self):
         for y in range(self.above_indent,screen_height,self.cell_size_y):
             pygame.draw.line(screen, black_color,(self.left_indent,y),(screen_width,y))
@@ -181,19 +181,47 @@ class Pics:
         self.white_pawn=pygame.transform.scale(self.white_pawn, (width, height))
         self.black_pawn = pygame.image.load("images/black_pawn.png")
         self.black_pawn=pygame.transform.scale(self.black_pawn, (width, height))
-
+def is_correct(pos):
+    if field.left_indent < pos[0] < screen_width and field.above_indent < pos[1] < screen_height:
+        return True
+    return False
+def coordinates_changer(pos):
+    new_x = (pos[0] - field.left_indent)//field.cell_size_x
+    new_y = (pos[1] - field.above_indent)//field.cell_size_y
+    return (new_x,new_y)
 def coordinates_changer_in_pygame(x,y):
     new_x = x*field.cell_size_x+field.left_indent
     new_y = y*field.cell_size_y+field.above_indent
     return(new_x,new_y)
+def is_touch_chessman(new_pos):
+    if field.field[new_pos[1]][new_pos[0]] !=0:
+        return True
+    return False
+def choose_chessman(pos):
+    return field.field[pos[1]][pos[0]]
 def events_check():
-    global process_running, scale, scale_value, objects,camera_center_x,camera_center_y,scale_number
+    global process_running, active_cell,active_chessman
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             process_running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 process_running = False
+        elif event.type== pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                pos = pygame.mouse.get_pos()
+                if is_correct(pos):
+                    new_pos = coordinates_changer(pos)
+                    if is_touch_chessman(new_pos):
+                        if not active_chessman:
+                            active_chessman = choose_chessman(new_pos)
+                        else:
+                            active_chessman=None
+                        if not active_cell:
+                            active_cell = new_pos
+                        else:
+                            active_cell = None
+                        print(active_chessman)
 def drawing():
     screen.fill(white_color)
     field.draw()
